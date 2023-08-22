5ca66fa290e99b41223e244a23065c6210af5e6a
القياده الخال شخصياً <115638470+Ali775624@users.noreply.github.com>
2023-08-23 00:33:50 +0300

    Update class.php
    # ابوت زالة الملابس من الصوره
    # ازالة الملابس من الصوره
    # ازالة الملابس من الصور 
    # بوت ازالة الملابس من الصور
    # telegram dress remover bot name
    # ai cloth remover
    # ai clothes remover
    # cloth remover ai
    # ai bot cloth remover
    # telegram ai girl image misuse
    # dress remover ai
    # ai dress remover app
    # ai bot cloth remover telegram bot
    # malai tech support
    # cloth remover ai bot telegram name
    
    [دخول للبوت](https://t.me/Driving_uncle_personally)
    [Login to the bot](https://t.me/Driving_uncle_personally)
    
    # cloth-remover-ai
    هاذي المدونه ستتيح لك الوصول الى بوت ازالة الملابس من الصوره فقط يمكنك الدخول عبر رابط القناه تليجرام واخذ رابط البوت لازالة الملابس من الصوره 🔞
    [دخول القناه تليجرام](https://t.me/Driving_uncle_personally)
    
    يمكنك متابعة الشرح يوتيوب عن كيفية استخدام البوت
    
    [مشاهدة الشرح يوتيوب](https://youtu.be/vdBwklElcZo)
    
    لا تنسى متابعة صفحتي على تيك توك
    [متابعة الصفحه تيك توك](https://www.tiktok.com/@alloush_alyamanl?_t=8XH4ksw51Ji&_r=1)
    
    
    او يمكنك متابعة حسابي انستجرام
    
    [متابعة الحساب انستجرام](https://www.instagram.com/aloush_aloush711?r=nametag)

--- class.php
+++ class.php
@@ -1,69 +1,34 @@
-<?php
+# ابوت زالة الملابس من الصوره
+# ازالة الملابس من الصوره
+# ازالة الملابس من الصور 
+# بوت ازالة الملابس من الصور
+# telegram dress remover bot name
+# ai cloth remover
+# ai clothes remover
+# cloth remover ai
+# ai bot cloth remover
+# telegram ai girl image misuse
+# dress remover ai
+# ai dress remover app
+# ai bot cloth remover telegram bot
+# malai tech support
+# cloth remover ai bot telegram name
+
+[دخول للبوت](https://t.me/Driving_uncle_personally)
+[Login to the bot](https://t.me/Driving_uncle_personally)
+
+# cloth-remover-ai
+هاذي المدونه ستتيح لك الوصول الى بوت ازالة الملابس من الصوره فقط يمكنك الدخول عبر رابط القناه تليجرام واخذ رابط البوت لازالة الملابس من الصوره 🔞
+[دخول القناه تليجرام](https://t.me/Driving_uncle_personally)
+
+يمكنك متابعة الشرح يوتيوب عن كيفية استخدام البوت
+
+[مشاهدة الشرح يوتيوب](https://youtu.be/vdBwklElcZo)
+
+لا تنسى متابعة صفحتي على تيك توك
+[متابعة الصفحه تيك توك](https://www.tiktok.com/@alloush_alyamanl?_t=8XH4ksw51Ji&_r=1)
 
 
-class _5sim_com {
-	
-	//private $api_key;
-	private $link = "http://api1.5sim.net/stubs/handler_api.php";
-	public function __construct ($api_key) {
-		//$this->api_key = $api_key;
-		$this->link .= "?api_key=" . $api_key;
-	}
-	//get balance
-	public function getBalance () {
-		$get = file_get_contents($this->link ."&action=getBalance");
-		$balance = explode (":",$get)[1] ?? 0;
-		return $this->isError ($get) ? 
-			["ok"=>true,"balance"=>$balance]:
-			["ok"=>false,"error"=>$get];
-	}
-	//get Number 
-	public function getNumber ($co,$ser) {
-		$get = file_get_contents($this->link ."&action=getNumber&service={$ser}&country={$co}");
-		$ex = explode (":",$get); 
-		$id = $ex[1]??0;
-		$number = $ex[2]??0;
-		return $this->isError ($get) ? 
-			["ok"=>true,"id"=>$id,"number"=>$number]:
-			["ok"=>false,"error"=>$get];
-	}
-	
-	//get code 
-	public function getCode ($id) {
-		//STATUS_OK:code 
-		$get = file_get_contents($this->link ."&action=getStatus&id={$id}");
-		$code = explode (":",$get)[1]??0;
-		return $this->isError ($get) ? 
-			["ok"=>true,"code"=>$code]:
-			["ok"=>false,"error"=>$get];
-	}
-	
-	//cancel number 
-	public function cencel ($id) {
-		$get = file_get_contents($this->link ."&action=setStatus&id={$id}&status=-1");
-		return $this->isError ($get) ? 
-			["ok"=>true]:
-			["ok"=>false,"error"=>$get];
-	}
-	// check if there is an error 
-	public function isError ($data) {
-		$listError = array(
-			"BAD_KEY",
-			"BAD_ACTION",
-			"NO_ACTIVATION",
-			"NO_NUMBERS",
-			"STATUS_WAIT_CODE",
-			"STATUS_CANCEL",
-			"NO_BALANCE",
-			"BAD_STATUS",
-			"STATUS_WAIT_RESEND",
-			"STATUS_WAIT_RETRY"
-		);
-		$preg = join ("|",$listError);
-		return !preg_match("/$preg/i",$data) ;
-	}
-}
+او يمكنك متابعة حسابي انستجرام
 
-
-
-
+[متابعة الحساب انستجرام](https://www.instagram.com/aloush_aloush711?r=nametag)
